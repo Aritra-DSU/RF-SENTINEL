205306b3ca0e49e960bccd0465197d047385291e
Aritra Banerjee <baritra369@gmail.com>
2026-04-25 00:19:15 +0530

    Update prepare_data.py

--- Ml_model/prepare_data.py
+++ Ml_model/prepare_data.py
@@ -2,13 +2,12 @@ import pandas as pd
 
 df = pd.read_csv("logged_data.csv")
 
-# 🔥 Normalize Frequency (VERY IMPORTANT)
 f_min = df["Frequency"].min()
 f_max = df["Frequency"].max()
 
 df["norm_freq"] = (df["Frequency"] - f_min) / (f_max - f_min) * 100
 
-# ✅ Create labels based on normalized freq
+
 def create_label(row):
     f = row["norm_freq"]
 
@@ -21,11 +20,11 @@ def create_label(row):
 
 df["label"] = df.apply(create_label, axis=1)
 
-# ✅ DEBUG (IMPORTANT)
+
 print("\nLabel Distribution:")
 print(df["label"].value_counts())
 
-# SAVE
+
 df.to_csv("labeled_data.csv", index=False)
 
-print("\n✅ labeled_data.csv created")
+print("\n labeled_data.csv created")
