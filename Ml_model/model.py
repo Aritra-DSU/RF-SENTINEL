e57de1f4b86228366637dbf260a42021556bb7cc
Aritra Banerjee <baritra369@gmail.com>
2026-04-25 00:18:34 +0530

    Refactor model.py to improve formatting

--- Ml_model/model.py
+++ Ml_model/model.py
@@ -28,7 +28,6 @@ model.fit(X_train, y_train)
 accuracy = model.score(X_test, y_test)
 print(f"✅ Model Accuracy: {accuracy * 100:.2f}%")
 
-# ✅ Save model
 joblib.dump(model, "model.pkl")
 
 print("✅ Model saved as model.pkl")
